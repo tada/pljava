f98602c6df35ad86e15b5a6e8743c1934f06c973
fhrbek <filip.hrbek@gmail.com>
2005-02-17 23:39:35 +0000

    Added initialization of new field m_class which is used in SyntheticResultSet

--- src/java/pljava/org/postgresql/pljava/jdbc/ResultSetField.java
+++ src/java/pljava/org/postgresql/pljava/jdbc/ResultSetField.java
@@ -6,6 +6,9 @@
  */
 package org.postgresql.pljava.jdbc;
 
+import java.sql.SQLException;
+import org.postgresql.pljava.internal.TypeMap;
+
 /**
  *
  * @author Filip Hrbek
@@ -17,6 +20,7 @@ public class ResultSetField
     private final int m_oid;
     private final int m_len;
     private final int m_mod;
+	private final Class m_class;
 
     /*
      * Construct a field based on the information fed to it.
@@ -26,11 +30,19 @@ public class ResultSetField
      * @param len the length of the field
      */
     public ResultSetField(String name, int oid, int len, int mod)
+	throws SQLException
     {
         m_name = name;
         m_oid = oid;
         m_len = len;
         m_mod = mod;
+		try {
+			m_class = Class.forName(TypeMap.getClassNameFromPgOid(oid));
+		}
+		catch (ClassNotFoundException e)
+		{
+			throw new SQLException(e.getMessage());
+		}
     }
 
     /*
@@ -41,6 +53,7 @@ public class ResultSetField
      * @param len the length of the field
      */
     public ResultSetField(String name, int oid, int len)
+	throws SQLException
     {
         this(name, oid, len, 0);
     }
@@ -51,6 +64,22 @@ public class ResultSetField
     public final int getOID()
     {
         return m_oid;
+    }
+
+    /*
+     * @return the Java class for oid of this Field's data type
+     */
+    public final Class getJavaClass()
+    {
+        return m_class;
+    }
+
+    /*
+     * @return true if the field can contain a value of specified class
+     */
+    public final boolean canContain(Class cls)
+    {
+        return m_class.isAssignableFrom(cls);
     }
 
     /*
