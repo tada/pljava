c81d586ac4826ed71aaa386da66351016fb4bff2
fhrbek <filip.hrbek@gmail.com>
2005-02-17 23:37:44 +0000

    Made better DatabaseMetaData testing functions.

--- src/java/examples/org/postgresql/pljava/example/MetaDataTest.java
+++ src/java/examples/org/postgresql/pljava/example/MetaDataTest.java
@@ -7,117 +7,282 @@
 package org.postgresql.pljava.example;
 
 import java.sql.*;
-import org.postgresql.pljava.ResultSetProvider;
+import java.lang.reflect.*;
+import java.util.regex.*;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 /**
- * @author Filip Hrbek Note: ResultSetProvider interface implementation is
- *         needed only for getTypeInfo method
+ * @author Filip Hrbek
  */
-public class MetaDataTest implements ResultSetProvider
+public class MetaDataTest
 {
-	public static String getDatabaseProductName() throws SQLException
+	public static Iterator callMetaDataMethod(String methodCall) throws SQLException
 	{
-		Connection conn = DriverManager
-			.getConnection("jdbc:default:connection");
-		DatabaseMetaData md = conn.getMetaData();
-		return md.getDatabaseProductName();
+		return new MetaDataTest(methodCall).iterator();
 	}
 
-	public static String getDatabaseProductVersion() throws SQLException
+	private ResultSet m_rs;
+	private String m_methodName;
+	private Object[] m_methodArgs;
+	private Class[] m_methodArgTypes;
+	private ArrayList m_results;
+
+	public MetaDataTest(String methodCall) throws SQLException
 	{
 		Connection conn = DriverManager
 			.getConnection("jdbc:default:connection");
 		DatabaseMetaData md = conn.getMetaData();
-		return md.getDatabaseProductVersion();
-	}
+		m_results = new ArrayList();
+		parseMethodCall(methodCall);
 
-	public static String getSchemas() throws SQLException
-	{
-		Connection conn = DriverManager
-			.getConnection("jdbc:default:connection");
-		DatabaseMetaData md = conn.getMetaData();
-		ResultSet rs = md.getSchemas();
-
-		StringBuffer buf = new StringBuffer();
-
-		while(rs.next())
+		try
 		{
-			buf.append(rs.getString(1) + ";");
+			Method m = DatabaseMetaData.class
+				.getMethod(m_methodName, m_methodArgTypes);
+			if (!m.getReturnType().equals(ResultSet.class))
+			{
+				throw new NoSuchMethodException("Unexpected return type");
+			}
+
+			m_rs = (ResultSet)m.invoke(md, m_methodArgs);
+
+			while (m_rs.next())
+			{
+				StringBuffer result = new StringBuffer();
+				Object rsObject = null;
+				int i=1;
+				while(true)
+				{
+					try
+					{
+						rsObject = m_rs.getObject(i++);
+						if (rsObject == null)
+						{
+							rsObject = "<NULL>";
+						}
+					}
+					//index out of bounds or SQL exception =>
+					//we have no other chance to find out
+					//number of columns until ResulSetMetaData
+					//is implemented
+					//So - let's break the loop!
+					catch (ArrayIndexOutOfBoundsException ae)
+					{
+						break;
+					}
+					catch (SQLException se)
+					{
+						break;
+					}
+					result.append(rsObject.toString()
+						.replaceAll("(\\\\|;)","\\$1") + ";");
+				}
+				m_results.add(result.toString());
+			}
+			m_rs.close();
 		}
-
-		rs.close();
-
-		return buf.toString();
-	}
-
-	public static String getTableTypes() throws SQLException
-	{
-		Connection conn = DriverManager
-			.getConnection("jdbc:default:connection");
-		DatabaseMetaData md = conn.getMetaData();
-		ResultSet rs = md.getTableTypes();
-
-		StringBuffer buf = new StringBuffer();
-
-		while(rs.next())
+		catch (NoSuchMethodException nme)
 		{
-			buf.append(rs.getString(1) + ";");
+			StringBuffer sb = new StringBuffer();
+			for (int i=0; i < m_methodArgTypes.length; i++)
+			{
+				if (sb.length() > 0)
+				{
+					sb.append(",");
+				}
+				sb.append(((Class)m_methodArgTypes[i]).getName());
+			}
+			throw new SQLException("No such method or non-resultset return type: " +
+									m_methodName +
+								   "(" + sb.toString() + ")");
 		}
-
-		rs.close();
-
-		return buf.toString();
-	}
-
-	ResultSet typeInfo;
-
-	public MetaDataTest() throws SQLException
-	{
-		Connection conn = DriverManager
-			.getConnection("jdbc:default:connection");
-		DatabaseMetaData md = conn.getMetaData();
-		typeInfo = md.getTypeInfo();
-	}
-
-	public boolean assignRowValues(ResultSet receiver, int currentRow)
-	throws SQLException
-	{
-		if(typeInfo.next())
+		catch (InvocationTargetException ite)
 		{
-			receiver.updateString(1, typeInfo.getString(1));
-			receiver.updateInt(2, typeInfo.getInt(2));
-			receiver.updateInt(3, typeInfo.getInt(3));
-			receiver.updateString(4, typeInfo.getString(4));
-			receiver.updateString(5, typeInfo.getString(5));
-			receiver.updateString(6, typeInfo.getString(6));
-			receiver.updateShort(7, typeInfo.getShort(7));
-			receiver.updateBoolean(8, typeInfo.getBoolean(8));
-			receiver.updateShort(9, typeInfo.getShort(9));
-			receiver.updateBoolean(10, typeInfo.getBoolean(10));
-			receiver.updateBoolean(11, typeInfo.getBoolean(11));
-			receiver.updateBoolean(12, typeInfo.getBoolean(12));
-			receiver.updateString(13, typeInfo.getString(13));
-			receiver.updateShort(14, typeInfo.getShort(14));
-			receiver.updateShort(15, typeInfo.getShort(15));
-			receiver.updateInt(16, typeInfo.getInt(16));
-			receiver.updateInt(17, typeInfo.getInt(17));
-			receiver.updateInt(18, typeInfo.getInt(18));
-
-			return true;
+			throw new SQLException(ite.getTargetException().toString());
 		}
-		return false;
+		catch (Exception e)
+		{
+			throw new SQLException("Method error: " + e.toString());
+		}
 	}
 
-	public static ResultSetProvider getTypeInfo() throws SQLException
+	private Iterator iterator()
+	{
+		return m_results.iterator();
+	}
+
+	/**
+	 * Method call parser.
+	 * Let's say that only String, String[], int, int[] and boolean types are accepted.
+	 * Examples:
+	 *    "foo" => String
+	 *    {"foo","bar"} => String[]
+	 *    {} => String[] (zero length array, not null!)
+	 *    10 => int
+	 *    -7 => int
+	 *    [10,3] => int[]
+	 *    [] => int[] (zero length array, not null!)
+	 *    TRUE => boolean
+	 *    (String)null => String (value null)
+	 *    (String[])null => String[] (value null)
+	 *    (int[])null => int[] (value null)
+	 *
+	 * Complete example:
+	 *    select * from callMetaDataMethod('getTables((String)null,"sqlj","jar%",{"TABLE"})');
+	 *
+	 * It makes the implementation simplier, and it is sufficient for
+	 * DatabaseMetaData testing.
+	 */
+	private void parseMethodCall(String methodCall) throws SQLException
 	{
 		try
 		{
-			return new MetaDataTest();
+			Pattern p = Pattern.compile("^\\s*([a-zA-Z_][a-zA-Z0-9_]*)\\s*\\((.*)\\)\\s*$");
+			Matcher m = p.matcher(methodCall);
+			String paramString;
+			String auxParamString;
+			String param;
+			ArrayList objects = new ArrayList();
+			ArrayList types = new ArrayList();
+
+			if (m.matches())
+			{
+				m_methodName = m.group(1);
+				paramString = m.group(2).trim();
+				p = Pattern.compile(
+					"^\\s*(" +
+					"\\(\\s*(?:String|int)\\s*(?:\\[\\s*\\])?\\s*\\)\\s*null|" + //String, String[] or int[] null
+					"TRUE|" +											 //boolean TRUE
+					"FALSE|" +											 //boolean FALSE
+					"(?:\\-|\\+)?[0-9]+|" +								 //int
+					"\\[((?:[^\\[\\]])*)\\]|" +							 //int[]
+					"\"((?:[^\\\\\"]|\\\\.)*)\"|" +						 //String
+					"\\{((?:[^\\{\\}]|\"(?:[^\\\\\"]|\\\\.)*\")*)\\}" +	 //String[]
+					")\\s*" +
+					"(?:,|$)" +											 //comma separator
+					"(.*)$");											 //rest of the string
+				auxParamString = paramString;
+				while(!auxParamString.equals(""))
+				{
+					m = p.matcher(auxParamString);
+					if (!m.matches())
+					{
+						throw new SQLException("Invalid parameter list: " + paramString);
+					}
+
+					param = m.group(1);
+					if (param.startsWith("\"")) //it is a string
+					{
+						param = m.group(3);    //string without the quotes
+						objects.add(param);
+						types.add(String.class);
+					}
+					else if (param.startsWith("{")) //it is a string array
+					{
+						param = m.group(4);    //string array without the curly brackets
+						Pattern parr = Pattern.compile("^\\s*\"((?:[^\\\\\"]|\\\\.)*)\"\\s*(?:,|$)(.*)$");
+						Matcher marr;
+						String auxParamArr = param.trim();
+						ArrayList strList = new ArrayList();
+						
+						while(!auxParamArr.equals(""))
+						{
+							marr = parr.matcher(auxParamArr);
+							if (!marr.matches())
+							{
+								throw new SQLException("Invalid string array: " + param);
+							}
+
+							strList.add(marr.group(1));
+							auxParamArr = marr.group(2).trim();
+						}
+						objects.add((String[])strList.toArray(new String[0]));
+						types.add(String[].class);
+					}
+					else if (param.equals("TRUE") || param.equals("FALSE")) //it is a boolean
+					{
+						objects.add(new Boolean(param));
+						types.add(Boolean.TYPE);
+					}
+					else if (param.startsWith("(")) //it is String, String[] or int[] null
+					{
+						Pattern pnull = Pattern.compile("^\\(\\s*(String|int)\\s*(\\[\\s*\\])?\\s*\\)\\s*null\\s*$");
+						Matcher mnull = pnull.matcher(param);
+
+						if (mnull.matches())
+						{
+							objects.add(null);
+							if (mnull.group(2) == null)
+							{
+								if (mnull.group(1).equals("String"))
+								{
+									types.add(String.class);
+								}
+								else
+								{
+									throw new SQLException("Primitive 'int' cannot be null");
+								}
+							}
+							else
+							{
+								if (mnull.group(1).equals("String"))
+								{
+									types.add(String[].class);
+								}
+								else
+								{
+									types.add(int[].class);
+								}
+							}
+						}
+						else
+						{
+							throw new SQLException("Invalid null value: " + param);
+						}
+					}
+					else if (param.startsWith("[")) //it is a int array
+					{
+						param = m.group(2);    //int array without the square brackets
+						Pattern parr = Pattern.compile("^\\s*(\\d+)\\s*(?:,|$)(.*)$");
+						Matcher marr;
+						String auxParamArr = param.trim();
+						ArrayList intList = new ArrayList();
+						
+						while(!auxParamArr.equals(""))
+						{
+							marr = parr.matcher(auxParamArr);
+							if (!marr.matches())
+							{
+								throw new SQLException("Invalid int array: " + param);
+							}
+
+							intList.add(new Integer(marr.group(1)));
+							auxParamArr = marr.group(2).trim();
+						}
+						objects.add((Integer[])intList.toArray(new Integer[0]));
+						types.add(int[].class);
+					}
+					else //it is an int
+					{
+						objects.add(new Integer(param));
+						types.add(Integer.TYPE);
+					}
+
+					auxParamString = m.group(5).trim();
+				}
+			}
+			else
+			{
+				throw new SQLException("Syntax error");
+			}
+
+			m_methodArgs = (Object[])objects.toArray(new Object[0]);
+			m_methodArgTypes = (Class[])types.toArray(new Class[0]);
 		}
-		catch(SQLException e)
+		catch (Exception e)
 		{
-			throw new SQLException("Error reading DatabaseMetaData", e
-				.getMessage());
+			throw new SQLException("Invalid method call: " + methodCall + ". Cause: " + e.toString());
 		}
 	}
 }
