3ae5d07b44d7cbc3ebdaec6e430388e253bcb54d
Jeff Shaw <jeff.shaw@wda.com>
2015-01-30 15:21:11 -0500

    Fix compilation error against 9.4 due to a missing function argument.

--- pljava-so/src/main/c/type/Oid.c
+++ pljava-so/src/main/c/type/Oid.c
@@ -216,7 +216,11 @@ Java_org_postgresql_pljava_internal_Oid__1forTypeName(JNIEnv* env, jclass cls, j
 		PG_TRY();
 		{
 			int32 typmod = 0;
+#if PGSQL_MAJOR_VER < 9 || (PGSQL_MAJOR_VER == 9 && PGSQL_MINOR_VER < 4)
 			parseTypeString(typeNameOrOid, &typeId, &typmod);
+#else
+			parseTypeString(typeNameOrOid, &typeId, &typmod, 0);
+#endif
 		}
 		PG_CATCH();
 		{
