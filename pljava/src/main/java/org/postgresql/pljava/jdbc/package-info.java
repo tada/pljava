db8131b79a553d2fab8a99bf565048403744cfc6
Chapman Flack <chap@anastigmatix.net>
2015-07-19 17:27:04 -0400

    Minimal package-info breadcrumbs scattered through rest of packages.

---
+++ pljava/src/main/java/org/postgresql/pljava/jdbc/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * PL/Java's specialized JDBC implementation that interacts directly with the
+ * backend over SPI.
+ * <p>
+ * Some classes and methods here have departures from strict JDBC behavior
+ * that should be covered in their javadoc, or resolved; for now,
+ * comments in the code may be helpful in case of any question.
+ * @author Thomas Hallgren
+ */
+package org.postgresql.pljava.jdbc;
