9bd31791943c0fc2f54a829533db626808ec8741
Chapman Flack <chap@anastigmatix.net>
2016-03-20 00:03:54 -0400

    Poke migration-management versions for 150b3.

--- pljava/src/main/java/org/postgresql/pljava/internal/InstallHelper.java
+++ pljava/src/main/java/org/postgresql/pljava/internal/InstallHelper.java
@@ -409,11 +409,11 @@ public class InstallHelper
 	 * up to date.
 	 */
 	private static final SchemaVariant currentSchema =
-		SchemaVariant.REL_1_5_0_BETA2;
+		SchemaVariant.REL_1_5_0_BETA3;
 
 	private enum SchemaVariant
 	{
-		REL_1_5_0_BETA2 ("c51cffa34acd5a228325143ec29563174891a873")
+		REL_1_5_0_BETA3 ("c51cffa34acd5a228325143ec29563174891a873")
 		{
 			@Override
 			void migrateFrom( SchemaVariant sv, Connection c, Statement s)
@@ -462,6 +462,7 @@ public class InstallHelper
 		UNREL20040120  ("5e4131738cd095b7ff6367d64f809f6cec6a7ba7"),
 		EMPTY          (null);
 
+		static final SchemaVariant REL_1_5_0_BETA2 = REL_1_5_0_BETA3;
 		static final SchemaVariant REL_1_5_0_BETA1 = REL_1_5_0_BETA2;
 		static final SchemaVariant UNREL20130301b = REL_1_5_0_BETA1;
 
