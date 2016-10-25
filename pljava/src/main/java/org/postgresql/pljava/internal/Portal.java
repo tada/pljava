5270b7bb680af94cd9aa68438650f5384dae2ab7
Chapman Flack <chap@anastigmatix.net>
2016-10-25 01:21:31 -0400

    Copyright notice in files just touched.
    Some files still have notices predating the explicit BSD-style one
    introduced in 2013. Update it in the files this branch has touched.

--- pljava/src/main/java/org/postgresql/pljava/internal/Portal.java
+++ pljava/src/main/java/org/postgresql/pljava/internal/Portal.java
@@ -1,8 +1,14 @@
 /*
- * Copyright (c) 2004, 2005, 2006 TADA AB - Taby Sweden
- * Distributed under the terms shown in the file COPYRIGHT
- * found in the root folder of this project or at
- * http://eng.tada.se/osprojects/COPYRIGHT.html
+ * Copyright (c) 2004-2016 Tada AB and other contributors, as listed below.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the The BSD 3-Clause License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Contributors:
+ *   Tada AB
+ *   Chapman Flack
  */
 package org.postgresql.pljava.internal;
 
