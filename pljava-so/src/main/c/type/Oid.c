1c76bab23c01afe61a3d2d514917b4bf8c126fec
Chapman Flack <chap@anastigmatix.net>
2018-04-21 10:35:15 -0400

    Mention JDBC 4.0 to 4.2 types in Oid.c.
    These are still not given any mapping other than to InvalidOid,
    but at least they are mentioned (conditionally, for the JDBC 4.2
    ones) in the switch statement now.

--- pljava-so/src/main/c/type/Oid.c
+++ pljava-so/src/main/c/type/Oid.c
@@ -103,13 +103,28 @@ Oid Oid_forSqlType(int sqlType)
 		case java_sql_Types_DATALINK:
 			typeId = TEXTOID;
 			break;
-/*		case java_sql_Types_NULL:
+		case java_sql_Types_NULL:
 		case java_sql_Types_OTHER:
 		case java_sql_Types_JAVA_OBJECT:
 		case java_sql_Types_DISTINCT:
 		case java_sql_Types_STRUCT:
 		case java_sql_Types_ARRAY:
-		case java_sql_Types_REF: */
+		case java_sql_Types_REF:
+
+		/* JDBC 4.0 - present in Java 6 and later, no need to conditionalize */
+		case java_sql_Types_ROWID:
+		case java_sql_Types_NCHAR:
+		case java_sql_Types_NVARCHAR:
+		case java_sql_Types_LONGNVARCHAR:
+		case java_sql_Types_NCLOB:
+		case java_sql_Types_SQLXML:
+
+		/* JDBC 4.2 - conditionalize until only Java 8 and later supported */
+#ifdef	java_sql_Types_REF_CURSOR
+		case java_sql_Types_REF_CURSOR:
+		case java_sql_Types_TIME_WITH_TIMEZONE:
+		case java_sql_Types_TIMESTAMP_WITH_TIMEZONE:
+#endif
 		default:
 			typeId = InvalidOid;	/* Not yet mapped */
 			break;
