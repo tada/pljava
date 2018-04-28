3ce8cbdf7c76d2f1b9c4439b24d2f025bd4816a7
Chapman Flack <chap@anastigmatix.net>
2018-04-28 19:13:52 -0400

    Add an example that applies a transform.

--- pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/PassXML.java
+++ pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/PassXML.java
@@ -19,8 +19,12 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -38,6 +42,8 @@ public class PassXML
 	static SQLXML s_sx;
 
 	static TransformerFactory s_tf = TransformerFactory.newInstance();
+
+	static Map<String,Templates> s_tpls = new HashMap<String,Templates>();
 
 	/**
 	 * Echo an XML parameter back as a string, exercising seven different ways
@@ -61,37 +67,73 @@ public class PassXML
 		return echoSQLXML(sx, how);
 	}
 
-	private static String echoSQLXML(SQLXML sx, int how) throws SQLException
+	/**
+	 * Precompile an XSL transform {@code source} and save it (for the
+	 * current session) as {@code name}.
+	 *<p>
+	 * Each value of {@code how}, 1-7, selects a different way of presenting
+	 * the {@code SQLXML} object to the XSL processor.
+	 *<p>
+	 * Preparing a transform with
+	 * {@link TransformerFactoory.newTemplates newTemplates()} seems to require
+	 * {@link Function.Trust.UNSANDBOXED Trust.UNSANDBOXED}, at least for the
+	 * XSLTC transform compiler in newer JREs.
+	 */
+	@Function(schema="javatest", trust=Function.Trust.UNSANDBOXED)
+	public static void prepareXMLTransform(String name, SQLXML source, int how)
+	throws SQLException
 	{
-		Source src;
-
-		switch ( how )
+		try
 		{
-			case 1:
-				src = new StreamSource(sx.getBinaryStream());
-				break;
-			case 2:
-				src = new StreamSource(sx.getCharacterStream());
-				break;
-			case 3:
-				src = new StreamSource(new StringReader(sx.getString()));
-				break;
-			case 4:
-				src = sx.getSource(DOMSource.class);
-				break;
-			case 5:
-				src = sx.getSource(SAXSource.class);
-				break;
-			case 6:
-				src = sx.getSource(StAXSource.class);
-				break;
-			case 7:
-				src = sx.getSource(StreamSource.class);
-				break;
-			default:
-				throw new SQLDataException("how should be 1-7", "22003");
+			s_tpls.put(name, s_tf.newTemplates(sxToSource(source, how)));
+		}
+		catch ( TransformerException te )
+		{
+			throw new SQLException("XML transformation failed", te);
+		}
+	}
+
+	@Function(schema="javatest")
+	public static String transformXML(
+		String transformName, SQLXML source, int how)
+	throws SQLException
+	{
+		Templates tpl = s_tpls.get(transformName);
+		Source src = sxToSource(source, how);
+		StringWriter sw = new StringWriter();
+		Result rlt = new StreamResult(sw);
+
+		try
+		{
+			Transformer t = tpl.newTransformer();
+			t.transform(src, rlt);
+		}
+		catch ( TransformerException te )
+		{
+			throw new SQLException("XML transformation failed", te);
 		}
 
+		return sw.toString();
+	}
+
+	private static Source sxToSource(SQLXML sx, int how) throws SQLException
+	{
+		switch ( how )
+		{
+			case  1: return new StreamSource(sx.getBinaryStream());
+			case  2: return new StreamSource(sx.getCharacterStream());
+			case  3: return new StreamSource(new StringReader(sx.getString()));
+			case  4: return     sx.getSource(DOMSource.class);
+			case  5: return     sx.getSource(SAXSource.class);
+			case  6: return     sx.getSource(StAXSource.class);
+			case  7: return     sx.getSource(StreamSource.class);
+			default: throw new SQLDataException("how should be 1-7", "22003");
+		}
+	}
+
+	private static String echoSQLXML(SQLXML sx, int how) throws SQLException
+	{
+		Source src = sxToSource(sx, how);
 		StringWriter sw = new StringWriter();
 		Result rlt = new StreamResult(sw);
 
