dd91fca7b92b9f4a541f4ce3b7e8d531b6ed79ef
Chapman Flack <chap@anastigmatix.net>
2020-01-11 22:00:49 -0500

    POM release specs and module-info files
    Produces a "successful" (well, completing) build, wherein the
    annotation processor never gets run, so no .ddr files get made.
    
    Nothing done yet to split the shaded pljava{,-api} jar into
    two modular jars, nor in Backend.c to supply them correctly.

---
+++ pljava-api/src/main/java/module-info.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020 Tada AB and other contributors, as listed below.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the The BSD 3-Clause License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Contributors:
+ *   Chapman Flack
+ */
+
+/**
+ * Defines the API for PL/Java.
+ */
+module org.postgresql.pljava
+{
+	requires java.base;
+	requires transitive java.sql;
+	requires java.compiler;
+
+	exports org.postgresql.pljava;
+	exports org.postgresql.pljava.annotation;
+	exports org.postgresql.pljava.sqlgen;
+
+	provides javax.annotation.processing.Processor
+		with org.postgresql.pljava.sqlgen.DDRProcessor;
+}
---
+++ pljava/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020 Tada AB and other contributors, as listed below.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the The BSD 3-Clause License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Contributors:
+ *   Chapman Flack
+ */
+
+/**
+ * Contains PL/Java's internal implementation.
+ */
+module org.postgresql.pljava.internal
+{
+	requires java.base;
+	requires java.management;
+	requires org.postgresql.pljava;
+
+	provides java.sql.Driver with org.postgresql.pljava.jdbc.SPIDriver;
+}
