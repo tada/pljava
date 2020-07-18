6a4611027a11c3cbc93e368728aaf0f0b1b0770a
Chapman Flack <chap@anastigmatix.net>
2020-07-18 17:16:06 -0400

    Don't rely on PowerShell's redirection
    Because PowerShell makes such a mess of stderr output from
    an external command, just setIn(out), and use
    redirectErrorStream(true) when starting outside processes.
    
    Do the file extraction from within jshell just by calling main(),
    rather than having that done in a separate earlier step with java -jar.

--- .appveyor/test_script.ps1
+++ .appveyor/test_script.ps1
@@ -1,32 +1,41 @@
 $packageJar = 'pljava-packaging' |
   Get-ChildItem -Recurse -Filter pljava-pg*.jar
+
 $packageJar = $packageJar.fullName
-java -jar $packageJar
+
 $mavenRepo = "$env:UserProfile\.m2\repository"
+
 $saxonVer = Join-Path $mavenRepo "net\sf\saxon\Saxon-HE" |
   Get-ChildItem
+
 $jdbcJar = Join-Path $mavenRepo "com\impossibl\pgjdbc-ng\pgjdbc-ng-all" |
   Get-ChildItem -Recurse -Filter pgjdbc-ng-all-*.jar
+
 $jdbcJar = $jdbcJar.fullName
+
 @'
 import static java.nio.file.Paths.get
 import java.sql.Connection
 import org.postgresql.pljava.packaging.Node
 import static org.postgresql.pljava.packaging.Node.qp
 
+System.setErr(System.out); // PowerShell makes a mess of stderr output
+
+Node.main(new String[0]); // Extract the files (with output to stdout)
+
 String vmopts = "-enableassertions:org.postgresql.pljava... -Xcheck:jni"
 
 Node n1 = Node.get_new_node("TestNode1")
 
 try (
-  AutoCloseable t1 = n1.initialized_cluster();
+  AutoCloseable t1 = n1.initialized_cluster(p->p.redirectErrorStream(true));
   AutoCloseable t2 = n1.started_server(Map.of(
     "client_min_messages", "info",
     "pljava.vmoptions", vmopts,
     "pljava.libjvm_location",
     get(System.getProperty("java.home"), "bin", "server", "jvm.dll")
       .toString()
-  ));
+  ), p->p.redirectErrorStream(true));
 )
 {
   try ( Connection c = n1.connect() )
