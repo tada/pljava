78fb7e01e6e73eacaec071d92a2d01bf4a491fb5
Chapman Flack <chap@anastigmatix.net>
2020-07-18 17:20:11 -0400

    A first attempt at using Node, through PowerShell
    Note to self: the yml mustn't have leading tabs; expand them to spaces.
    
    What happens when an external command writes info on stderr (even
    innocuous progress info) is not pretty; PowerShell has a hyperliteral
    idea of what 'stderr' is for, and believes anything written there
    should be shown in red and converted to Error Records.
    
    The PowerShell 2>&1 operator, as much as it looks like the POSIX one,
    isn't a lot better. It does join the content into the output stream,
    where it no longer appears in red, but only after still trying to make
    Error Records out of it anyway.
    
    Discussion: https://help.appveyor.com/discussions/questions/48697

--- .appveyor/appveyor_mingw.sh
+++ .appveyor/appveyor_mingw.sh
@@ -4,4 +4,4 @@ PATH=$JAVA_HOME/bin:$PATH
 javac -version
 pacman -S mingw-w64-x86_64-postgresql --noconfirm
 cd /c/projects/pljava
-mvn clean install -Dnar.cores=1 -Psaxon-examples -Pwnosign --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+mvn clean install -Dnar.cores=1 -Psaxon-examples -Ppgjdbc-ng -Pwnosign --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
