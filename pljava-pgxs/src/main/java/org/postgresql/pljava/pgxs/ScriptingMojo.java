8eb132f27a63f717eda11499a86c684a74f3e0d4
Chapman Flack <chap@anastigmatix.net>
2020-07-19 16:15:41 -0400

    Try a 2-parent classloader for ScriptEngineManager
    My understanding (since 6cd539b) has been that Java 9 moved Nashorn
    from the bootstrap classloader to the application classloader [1], and
    that Maven creates classloaders that do not delegate to the application
    classloader (a/k/a the system classloader, as various ClassLoader
    methods still call it).
    
    So, what seemed an easy solution would be to create a new ClassLoader
    as a child of the Maven-supplied loader, but that would also behave as
    if it had the application class loader as a second parent.
    
    It still doesn't find Nashorn though. Back to the drawing board.
    
    [1] https://issues.apache.org/jira/browse/MANTRUN-200?focusedCommentId=16138712&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-16138712

--- pljava-pgxs/src/main/java/org/postgresql/pljava/pgxs/ScriptingMojo.java
+++ pljava-pgxs/src/main/java/org/postgresql/pljava/pgxs/ScriptingMojo.java
@@ -23,12 +23,79 @@ import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.List;
 
 @Mojo(name = "scripting", defaultPhase = LifecyclePhase.INITIALIZE,
       requiresDependencyResolution = ResolutionScope.TEST)
 public class ScriptingMojo extends AbstractMojo
 {
+	/**
+	 * A {@code ClassLoader} with (effectively) two parents, the inherited one
+	 * and Java's application class loader.
+	 *<p>
+	 * This loader will be given to the {@code ScriptEngineManager}. The
+	 * inherited loader supplied by Maven omits the application class loader,
+	 * to insulate Maven builds from possible differences in the application
+	 * class path; a reasonable idea, but the Nashorn script engine was moved to
+	 * the application class path as of Java 9, so without checking there, we
+	 * will not find it. This loader (like most) delegates first to its parent,
+	 * which should be the Maven-supplied loader; only what is not found there
+	 * will be sought from the application loader.
+	 */
+	static class ScriptEngineLoader extends ClassLoader
+	{
+		private ScriptEngineLoader(ClassLoader parent)
+		{
+			super("pgxsScriptLoader", parent);
+		}
+
+		/**
+		 * Delegate to the application loader.
+		 *<p>
+		 * This is called by the {@code super} implementation of
+		 * {@code loadClass} only after the parent loader has drawn a blank,
+		 * so there is nothing left to do but see if the application loader
+		 * has the class.
+		 */
+		@Override
+		protected Class<?> findClass(String name) throws ClassNotFoundException
+		{
+			return findSystemClass(name);
+		}
+
+		/**
+		 * Delegate to the application loader for finding a resource.
+		 *<p>
+		 * This is called by the {@code super} implementation of
+		 * {@code getResource} only after the parent loader has drawn a blank,
+		 * so there is nothing left to do but see if the application loader
+		 * has the resource.
+		 */
+		@Override
+		protected URL findResource(String name)
+		{
+			return getSystemResource(name);
+		}
+
+		/**
+		 * Delegate to the application loader for finding a resource.
+		 *<p>
+		 * This is called by the {@code super} implementation of
+		 * {@code getResources} after enumerating the resources available from
+		 * the parent loader. This method needs only to return the resources
+		 * available from the application loader; the caller will combine the
+		 * two enumerations.
+		 */
+		@Override
+		protected Enumeration<URL> findResources(String name) throws IOException
+		{
+			return getSystemResources(name);
+		}
+	}
+
 	ScriptEngine engine;
 
 	@Parameter(defaultValue = "${project}", required = true, readonly = true)
@@ -44,7 +111,7 @@ public class ScriptingMojo extends AbstractMojo
 	public void execute ()
 	{
 		ScriptEngineManager manager = new ScriptEngineManager(
-			ClassLoader.getSystemClassLoader());
+			new ScriptEngineLoader(ScriptingMojo.class.getClassLoader()));
 		engine = manager.getEngineByName("JavaScript");
 
 		getLog().debug(engine.toString());
