6e39cff36d1b941d955ca92c589b2b4c10c10ede
Kartik Ohri <kartikohri13@gmail.com>
2020-07-27 16:02:53 +0530

    Add JavadocMojo for executing the javadoc tool during site phase

---
+++ pljava-pgxs/src/main/java/org/postgresql/pljava/pgxs/JavadocMojo.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2020 Tada AB and other contributors, as listed below.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the The BSD 3-Clause License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Contributors:
+ *   Kartik Ohri
+ */
+package org.postgresql.pljava.pgxs;
+
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
+import org.codehaus.plexus.configuration.PlexusConfiguration;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+@Mojo(name = "generate-javadoc")
+@Execute(phase = LifecyclePhase.NONE)
+public class JavadocMojo extends AbstractMavenReport
+{
+	@Parameter
+	private PlexusConfiguration script;
+
+	private final List<String> javadocArguments = new ArrayList<>();
+
+	@Override
+	public String getOutputName ()
+	{
+		return "'PL/Java' Javadoc Report";
+	}
+
+	@Override
+	public String getName (Locale locale)
+	{
+		return String.format(locale, "%s", "PL/Java Javadoc Report");
+	}
+
+	@Override
+	public String getDescription (Locale locale)
+	{
+		return "Javadoc Generation Goal";
+	}
+
+	@Override
+	protected void executeReport (Locale locale) throws MavenReportException
+	{
+		try
+		{
+			ScriptEngine engine = PGXSUtils.getScriptEngine(script, getLog());
+			String scriptText = script.getValue();
+			getLog().error(scriptText);
+
+			engine.getContext().setAttribute("report", this,
+			                                 ScriptContext.GLOBAL_SCOPE);
+			engine.put("addJavadocArgument",
+			           (Consumer<String>) this::addJavadocArgument);
+			engine.eval(scriptText);
+
+			PGXSUtils.executeJavadocTool("javadoc", javadocArguments);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void addJavadocArgument(String argument)
+	{
+		javadocArguments.add(argument);
+	}
+
+}
