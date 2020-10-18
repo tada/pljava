63bea945a3073d5dc0287f18fe14d3f85f21a362
Chapman Flack <chap@anastigmatix.net>
2020-10-18 18:45:31 -0400

    Poke migration-management versions for 1.6.0

--- pljava/src/main/java/org/postgresql/pljava/internal/InstallHelper.java
+++ pljava/src/main/java/org/postgresql/pljava/internal/InstallHelper.java
@@ -668,6 +668,8 @@ public class InstallHelper
 		UNREL20040120  ("5e4131738cd095b7ff6367d64f809f6cec6a7ba7"),
 		EMPTY          (null);
 
+		static final SchemaVariant REL_1_6_0       = REL_1_5_0;
+
 		static final SchemaVariant REL_1_5_6       = REL_1_5_0;
 		static final SchemaVariant REL_1_5_5       = REL_1_5_0;
 		static final SchemaVariant REL_1_5_4       = REL_1_5_0;
