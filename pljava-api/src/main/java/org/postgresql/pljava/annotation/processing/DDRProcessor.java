5f34cef0092324ad192057f94ae6ce6fe67c34ff
Chapman Flack <chap@anastigmatix.net>
2023-05-29 21:56:07 -0400

    Overlooked copyright year.

--- pljava-api/src/main/java/org/postgresql/pljava/annotation/processing/DDRProcessor.java
+++ pljava-api/src/main/java/org/postgresql/pljava/annotation/processing/DDRProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2004-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -9,6 +9,7 @@
  * Contributors:
  *   Tada AB
  *   Purdue University
+ *   Chapman Flack
  */
 package org.postgresql.pljava.annotation.processing;
 
