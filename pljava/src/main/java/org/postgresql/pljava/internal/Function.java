e124899bed5197f4b76aaf2298abe0d8f077f070
Chapman Flack <chap@anastigmatix.net>
2023-06-07 17:25:11 -0400

    Bump copyright year

--- pljava/src/main/java/org/postgresql/pljava/internal/Function.java
+++ pljava/src/main/java/org/postgresql/pljava/internal/Function.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2022 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2016-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
