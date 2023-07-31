1423366b909c4bd8b17b9f9753612ca85ed912ae
Chapman Flack <chap@anastigmatix.net>
2023-07-30 23:40:35 -0400

    Add an example for multi-array retrieval

--- pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/TupleTableSlotTest.java
+++ pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/TupleTableSlotTest.java
@@ -15,7 +15,13 @@ import java.sql.Connection;
 import static java.sql.DriverManager.getConnection;
 import java.sql.SQLException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.util.Arrays.deepToString;
+import java.util.Iterator;
 import java.util.List;
+
+import java.time.OffsetDateTime;
 
 import org.postgresql.pljava.Adapter;
 import org.postgresql.pljava.Adapter.As;
@@ -40,6 +46,93 @@ import org.postgresql.pljava.model.TupleTableSlot;
  */
 public class TupleTableSlotTest
 {
+	/**
+	 * Test retrieval of a PostgreSQL array as a multidimensional Java array.
+	 */
+	@Function(schema="javatest")
+	public static Iterator<String> javaMultiArrayTest()
+	throws SQLException, ReflectiveOperationException
+	{
+		Connection c = getConnection("jdbc:default:connection");
+		SlotTester t = c.unwrap(SlotTester.class);
+
+		/*
+		 * First obtain an Adapter for the component type. For now, the untyped
+		 * adapterPlease method is needed to grovel it out of PL/Java's innards
+		 * and then cast it. An adapter manager with proper generic typing will
+		 * handle that part some day.
+		 */
+		AsLong<?> int8 = (AsLong<?>)t.adapterPlease(
+			"org.postgresql.pljava.pg.adt.Primitives", "INT8_INSTANCE");
+		AsInt<?> int4 = (AsInt<?>)t.adapterPlease(
+			"org.postgresql.pljava.pg.adt.Primitives", "INT4_INSTANCE");
+		AsShort<?> int2 = (AsShort<?>)t.adapterPlease(
+			"org.postgresql.pljava.pg.adt.Primitives", "INT2_INSTANCE");
+		AsByte<?> int1 = (AsByte<?>)t.adapterPlease(
+			"org.postgresql.pljava.pg.adt.Primitives", "INT1_INSTANCE");
+		AsDouble<?> float8 = (AsDouble<?>)t.adapterPlease(
+			"org.postgresql.pljava.pg.adt.Primitives", "FLOAT8_INSTANCE");
+		AsFloat<?> float4 = (AsFloat<?>)t.adapterPlease(
+			"org.postgresql.pljava.pg.adt.Primitives", "FLOAT4_INSTANCE");
+		AsBoolean<?> bool = (AsBoolean<?>)t.adapterPlease(
+			"org.postgresql.pljava.pg.adt.Primitives", "BOOLEAN_INSTANCE");
+
+		As<OffsetDateTime,?> odt = (As<OffsetDateTime,?>)t.adapterPlease(
+			"org.postgresql.pljava.pg.adt.DateTimeAdapter$JSR310",
+			"TIMESTAMPTZ_INSTANCE");
+
+		/*
+		 * Once properly-typed adapters for component types are in hand,
+		 * getting properly-typed array adapters is straightforward.
+		 * (Java 10+ var can reduce verbosity here.)
+		 */
+		As<          long[]           ,?> i8x1 = int8            .a1().build();
+		As<           int[][]         ,?> i4x2 = int4       .a2()     .build();
+		As<         short[][][]       ,?> i2x3 = int2       .a2().a1().build();
+		As<          byte[][][][]     ,?> i1x4 = int1  .a4()          .build();
+		As<        double[][][][][]   ,?> f8x5 = float8.a4()     .a1().build();
+		As<         float[][][][][][] ,?> f4x6 = float4.a4().a2()     .build();
+		As<       boolean[][][][][]   ,?>  bx5 = bool  .a4()     .a1().build();
+		As<OffsetDateTime[][][][]     ,?> dtx4 = odt   .a4()          .build();
+
+		String query =
+			"SELECT" +
+			" '{1,2}'::int8[], " +
+			" '{{1},{2}}'::int4[], " +
+			" '{{{1,2,3}}}'::int2[], " +
+			" '{{{{1},{2},{3}}}}'::\"char\"[], " +
+			" '{{{{{1,2,3}}}}}'::float8[], " +
+			" '{{{{{{1},{2},{3}}}}}}'::float4[], " +
+			" '{{{{{t},{f},{t}}}}}'::boolean[], " +
+			" '{{{{''now''}}}}'::timestamptz[]";
+
+		List<TupleTableSlot> tups = t.test(query);
+
+		List<String> result = new ArrayList<>();
+
+		/*
+		 * Then just pass the right adapter to tts.get.
+		 */
+		for ( TupleTableSlot tts : tups )
+		{
+			long           []           v0 = tts.get(0, i8x1);
+			int            [][]         v1 = tts.get(1, i4x2);
+			short          [][][]       v2 = tts.get(2, i2x3);
+			byte           [][][][]     v3 = tts.get(3, i1x4);
+			double         [][][][][]   v4 = tts.get(4, f8x5);
+			float          [][][][][][] v5 = tts.get(5, f4x6);
+			boolean        [][][][][]   v6 = tts.get(6,  bx5);
+			OffsetDateTime [][][][]     v7 = tts.get(7, dtx4);
+
+			result.addAll(List.of(
+				Arrays.toString(v0), deepToString(v1), deepToString(v2),
+				   deepToString(v3), deepToString(v4), deepToString(v5),
+				   deepToString(v6), deepToString(v7)));
+		}
+
+		return result.iterator();
+	}
+
 	/**
 	 * A temporary test jig during TupleTableSlot development; intended
 	 * to be used from a debugger.
