fd632d4d0498c8e52d3f61320c6f8ace7b3ec848
Chapman Flack <chap@anastigmatix.net>
2023-09-12 14:29:31 -0400

    Try Array type replacing element type if array
    This is idea 2 from the preceding commit message.
    
    That looked easy enough, but the simple reference equality
    test Type_getElementType(self) == other fails, because it
    turns out there can be more than one Type object (even
    pointing to different TypeClass objects!) for the same array
    type, and that turns out to be because of a change three
    years ago in 19afa36, whose commit message should be read
    in full for context.
    
    Back to the drawing board.

--- pljava-so/src/main/c/type/Array.c
+++ pljava-so/src/main/c/type/Array.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2020 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2004-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -157,7 +157,8 @@ static bool _Array_canReplaceType(Type self, Type other)
 	if ( oe == 0 )
 		return false;
 	return Type_canReplaceType(Type_getElementType(self), oe)
-		|| Type_getObjectType(self) == other;
+		|| Type_getObjectType(self) == other
+		|| Type_getElementType(self) == other;
 }
 
 Type Array_fromOid(Oid typeId, Type elementType)
