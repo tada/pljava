tada/pljava

commit 17b7d7cc70a521b490ec09498d68567a9b282761

PG compatibility horizon to 9.5
PostgreSQL 9.5 has been advertised as the oldest supported
version for PL/Java 1.6.x since 1.6.0. Declutter by removing
conditional code lingering from support of older versions.

In passing, add a couple SPI constants that showed up later
while my back was turned.

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/ConditionalDDR.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2020 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2015-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -64,7 +64,7 @@ import org.postgresql.pljava.annotation.SQLAction;
  * local, so it is reverted when the transaction completes.
  * <p>
  * In addition to the goodness-of-life examples, this file also generates
- * several statements setting PostgreSQL-version-based implementor tags that
+ * one or more statements setting PostgreSQL-version-based implementor tags that
  * are relied on by various other examples in this directory.
  */
 @SQLAction(provides={"LifeIsGood","LifeIsNotGood"}, install=
@@ -78,51 +78,12 @@ import org.postgresql.pljava.annotation.SQLAction;
 )
 
 @SQLAction(implementor="LifeIsGood", install=
-	"SELECT javatest.logmessage('INFO', 'Looking good!')"
+	"SELECT javatest.logmessage('INFO', 'ConditionlDDR looking good!')"
 )
 
 @SQLAction(implementor="LifeIsNotGood", install=
-	"SELECT javatest.logmessage('WARNING', 'This should not be executed')"
-)
-
-@SQLAction(provides="postgresql_ge_80300", install=
-	"SELECT CASE WHEN" +
-	" 80300 <= CAST(current_setting('server_version_num') AS integer)" +
-	" THEN set_config('pljava.implementors', 'postgresql_ge_80300,' || " +
-	" current_setting('pljava.implementors'), true) " +
-	"END"
-)
-
-@SQLAction(provides="postgresql_ge_80400", install=
-	"SELECT CASE WHEN" +
-	" 80400 <= CAST(current_setting('server_version_num') AS integer)" +
-	" THEN set_config('pljava.implementors', 'postgresql_ge_80400,' || " +
-	" current_setting('pljava.implementors'), true) " +
-	"END"
-)
-
-@SQLAction(provides="postgresql_ge_90000", install=
-	"SELECT CASE WHEN" +
-	" 90000 <= CAST(current_setting('server_version_num') AS integer)" +
-	" THEN set_config('pljava.implementors', 'postgresql_ge_90000,' || " +
-	" current_setting('pljava.implementors'), true) " +
-	"END"
-)
-
-@SQLAction(provides="postgresql_ge_90100", install=
-	"SELECT CASE WHEN" +
-	" 90100 <= CAST(current_setting('server_version_num') AS integer)" +
-	" THEN set_config('pljava.implementors', 'postgresql_ge_90100,' || " +
-	" current_setting('pljava.implementors'), true) " +
-	"END"
-)
-
-@SQLAction(provides="postgresql_ge_90300", install=
-	"SELECT CASE WHEN" +
-	" 90300 <= CAST(current_setting('server_version_num') AS integer)" +
-	" THEN set_config('pljava.implementors', 'postgresql_ge_90300,' || " +
-	" current_setting('pljava.implementors'), true) " +
-	"END"
+	"SELECT javatest.logmessage('WARNING', " +
+	" 'ConditionalDDR: This should not be executed')"
 )
 
 @SQLAction(provides="postgresql_ge_100000", install=

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/Enumeration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2020 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2015-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -21,16 +21,12 @@ import org.postgresql.pljava.annotation.Function;
 /**
  * Confirms the mapping of PG enum and Java String, and arrays of each, as
  * parameter and return types.
- *<p>
- * This example relies on {@code implementor} tags reflecting the PostgreSQL
- * version, set up in the {@link ConditionalDDR} example. PostgreSQL before 8.3
- * did not have enum types.
  */
-@SQLAction(provides="mood type", implementor="postgresql_ge_80300",
+@SQLAction(provides="mood type",
 	install="CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy')",
 	remove="DROP TYPE mood"
 )
-@SQLAction(implementor="postgresql_ge_80300",
+@SQLAction(
 	requires={"textToMood", "moodToText", "textsToMoods", "moodsToTexts"},
 	install={
 		"SELECT textToMood('happy')",
@@ -41,26 +37,22 @@ import org.postgresql.pljava.annotation.Function;
 )
 public class Enumeration
 {
-	@Function(requires="mood type", provides="textToMood", type="mood",
-			  implementor="postgresql_ge_80300")
+	@Function(requires="mood type", provides="textToMood", type="mood")
 	public static String textToMood(String s)
 	{
 		return s;
 	}
-	@Function(requires="mood type", provides="moodToText",
-			  implementor="postgresql_ge_80300")
+	@Function(requires="mood type", provides="moodToText")
 	public static String moodToText(@SQLType("mood")String s)
 	{
 		return s;
 	}
-	@Function(requires="mood type", provides="textsToMoods", type="mood",
-			  implementor="postgresql_ge_80300")
+	@Function(requires="mood type", provides="textsToMoods", type="mood")
 	public static Iterator<String> textsToMoods(String[] ss)
 	{
 		return Arrays.asList(ss).iterator();
 	}
-	@Function(requires="mood type", provides="moodsToTexts",
-			  implementor="postgresql_ge_80300")
+	@Function(requires="mood type", provides="moodsToTexts")
 	public static Iterator<String> moodsToTexts(@SQLType("mood[]")String[] ss)
 	{
 		return Arrays.asList(ss).iterator();

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/JDBC42_21.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2022 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2018-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -14,20 +14,15 @@ package org.postgresql.pljava.example.annotation;
 import org.postgresql.pljava.annotation.Function;
 import org.postgresql.pljava.annotation.SQLAction;
 
-import org.postgresql.pljava.example.annotation.ConditionalDDR; // for javadoc
-
 /**
  * Exercise new mappings between date/time types and java.time classes
  * (JDBC 4.2 change 21).
  *<p>
  * Defines a method {@link #javaSpecificationGE javaSpecificationGE} that may be
  * of use for other examples.
- *<p>
- * Relies on PostgreSQL-version-specific implementor tags set up in the
- * {@link ConditionalDDR} example.
  */
 @SQLAction(
-	implementor="postgresql_ge_90300",requires="TypeRoundTripper.roundTrip",
+	requires="TypeRoundTripper.roundTrip",
 	install={
 	" SELECT" +
 	"  CASE WHEN every(orig = roundtripped)" +

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/PGF1010962.java
@@ -10,12 +10,8 @@ import org.postgresql.pljava.annotation.SQLAction;
 /**
  * A gnarly test of TupleDesc reference management, crafted by Johann Oskarsson
  * for bug report 1010962 on pgFoundry.
- *<p>
- * This example relies on {@code implementor} tags reflecting the PostgreSQL
- * version, set up in the {@link ConditionalDDR} example. Before PostgreSQL 8.4,
- * there is no array of {@code RECORD}, which this test requires.
  */
-@SQLAction(requires="1010962 func", implementor="postgresql_ge_80400",
+@SQLAction(requires="1010962 func",
 	install={
 		"CREATE TYPE javatest.B1010962 AS ( b1_val float8, b2_val int)",
 
@@ -51,8 +47,7 @@ public class PGF1010962
 	 * @param receiver Looks polymorphic, but expects an array of A1010962
 	 * @return 0
 	 */
-	@Function(schema="javatest", provides="1010962 func",
-				implementor="postgresql_ge_80400")
+	@Function(schema="javatest", provides="1010962 func")
 	public static int complexParam( ResultSet receiver[] )
 	throws SQLException
 	{

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/PassXML.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2021 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2018-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -112,16 +112,7 @@ import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 	"END"
 )
 
-@SQLAction(implementor="postgresql_ge_80400",
-	provides="postgresql_xml_ge84",
-	install=
-	"SELECT CASE (SELECT 1 FROM pg_type WHERE typname = 'xml') WHEN 1" +
-	" THEN set_config('pljava.implementors', 'postgresql_xml_ge84,' || " +
-	" current_setting('pljava.implementors'), true) " +
-	"END"
-)
-
-@SQLAction(implementor="postgresql_xml_ge84", requires="echoXMLParameter",
+@SQLAction(implementor="postgresql_xml", requires="echoXMLParameter",
 	install=
 	"WITH" +
 	" s(how) AS (SELECT generate_series(1, 7))," +
@@ -146,7 +137,7 @@ import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 	" r"
 )
 
-@SQLAction(implementor="postgresql_xml_ge84", requires="proxiedXMLEcho",
+@SQLAction(implementor="postgresql_xml", requires="proxiedXMLEcho",
 	install=
 	"WITH" +
 	" s(how) AS (SELECT unnest('{1,2,4,5,6,7}'::int[]))," +
@@ -170,7 +161,7 @@ import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 	" r"
 )
 
-@SQLAction(implementor="postgresql_xml_ge84", requires="lowLevelXMLEcho",
+@SQLAction(implementor="postgresql_xml", requires="lowLevelXMLEcho",
 	install={
 	"SELECT" +
 	" preparexmlschema('schematest', $$" +
@@ -702,7 +693,7 @@ public class PassXML implements SQLData
 	 * still be exercised by calling this method, explicitly passing
 	 * {@code adjust => NULL}.
 	 */
-	@Function(schema="javatest", implementor="postgresql_xml_ge84",
+	@Function(schema="javatest", implementor="postgresql_xml",
 		provides="lowLevelXMLEcho")
 	public static SQLXML lowLevelXMLEcho(
 		SQLXML sx, int how, @SQLType(defaultValue={}) ResultSet adjust)
@@ -1046,12 +1037,9 @@ public class PassXML implements SQLData
 
 	/**
 	 * Text-typed variant of lowLevelXMLEcho (does not require XML type).
-	 *<p>
-	 * It does declare a parameter default, limiting it to PostgreSQL 8.4 or
-	 * later.
 	 */
 	@Function(schema="javatest", name="lowLevelXMLEcho",
-		type="text", implementor="postgresql_ge_80400")
+		type="text")
 	public static SQLXML lowLevelXMLEcho_(@SQLType("text") SQLXML sx, int how,
 		@SQLType(defaultValue={}) ResultSet adjust)
 	throws SQLException

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/PreJSR310.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2021 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2018-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -29,9 +29,6 @@ import org.postgresql.pljava.annotation.SQLAction;
  * Some tests of pre-JSR 310 date/time/timestamp conversions.
  *<p>
  * For now, just {@code java.sql.Date}, thanks to issue #199.
- *<p>
- * This example relies on {@code implementor} tags reflecting the PostgreSQL
- * version, set up in the {@link ConditionalDDR} example.
  */
 @SQLAction(provides="language java_tzset", install={
 	"SELECT sqlj.alias_java_language('java_tzset', true)"
@@ -39,7 +36,7 @@ import org.postgresql.pljava.annotation.SQLAction;
 	"DROP LANGUAGE java_tzset"
 })
 
-@SQLAction(implementor="postgresql_ge_90300", // needs LATERAL
+@SQLAction(
 	requires="issue199", install={
 	"SELECT javatest.issue199()"
 })

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/RecordParameterDefaults.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2018-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -28,9 +28,6 @@ import org.postgresql.pljava.annotation.SQLType;
  * function.
  *<p>
  * Also tests the proper DDR generation of defaults for such parameters.
- *<p>
- * This example relies on {@code implementor} tags reflecting the PostgreSQL
- * version, set up in the {@link ConditionalDDR} example.
  */
 @SQLAction(
 	provides = "paramtypeinfo type", // created in Triggers.java
@@ -64,7 +61,6 @@ public class RecordParameterDefaults implements ResultSetProvider
 	@Function(
 		requires = "paramtypeinfo type",
 		schema = "javatest",
-		implementor = "postgresql_ge_80400", // supports function param DEFAULTs
 		type = "javatest.paramtypeinfo"
 		)
 	public static ResultSetProvider paramDefaultsRecord(
@@ -87,7 +83,6 @@ public class RecordParameterDefaults implements ResultSetProvider
 	 */
 	@Function(
 		requires = "foobar tables", // created in Triggers.java
-		implementor = "postgresql_ge_80400", // supports function param DEFAULTs
 		schema = "javatest"
 		)
 	public static String paramDefaultsNamedRow(

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/SetOfRecordTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2020 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2004-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -26,12 +26,8 @@ import org.postgresql.pljava.annotation.SQLAction;
  * Example implementing the {@code ResultSetHandle} interface, to return
  * the {@link ResultSet} from any SQL {@code SELECT} query passed as a string
  * to the {@link #executeSelect executeSelect} function.
- *<p>
- * This example relies on {@code implementor} tags reflecting the PostgreSQL
- * version, set up in the {@link ConditionalDDR} example. Before PostgreSQL 8.4,
- * there was no {@code =} or {@code DISTINCT FROM} operator between row types.
  */
-@SQLAction(requires="selecttorecords fn", implementor="postgresql_ge_80400",
+@SQLAction(requires="selecttorecords fn",
 install=
 " SELECT " +
 "  CASE WHEN r IS DISTINCT FROM ROW('Foo'::varchar, 1::integer, 1.5::float, " +

commit 117f8e2b86f2e61df148091a1961c8d7d887b83c

Fix message typo in an example
Introduced in 17b7d7c.

pljava-examples/src/main/java/org/postgresql/pljava/example/annotation/ConditionalDDR.java
@@ -78,7 +78,7 @@ import org.postgresql.pljava.annotation.SQLAction;
 )
 
 @SQLAction(implementor="LifeIsGood", install=
-	"SELECT javatest.logmessage('INFO', 'ConditionlDDR looking good!')"
+	"SELECT javatest.logmessage('INFO', 'ConditionalDDR looking good!')"
 )
 
 @SQLAction(implementor="LifeIsNotGood", install=