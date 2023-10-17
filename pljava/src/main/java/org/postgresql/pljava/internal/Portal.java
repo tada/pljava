9a1f669eabd96e1453bed5aa29266a8f3be2395f
Chapman Flack <chap@anastigmatix.net>
2023-10-17 14:58:29 -0400

    Copyright year

--- pljava/src/main/java/org/postgresql/pljava/internal/Portal.java
+++ pljava/src/main/java/org/postgresql/pljava/internal/Portal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2019 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2004-2023 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
