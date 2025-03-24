18eb26c057ffe55c9672438b4cdaf0a451fcab9c
Chapman Flack <chap@anastigmatix.net>
2025-03-24 18:37:08 -0400

    PG17: infinitely negative or positive INTERVALs
    Handled in this commit merely as a refinement of javadoc comments.
    
    However, also corrected initialization of the static final Field sets:
    it's an unwelcome surprise that while Set.of returns an immutable set,
    EnumSet.of does not.

--- pljava-api/src/main/java/org/postgresql/pljava/adt/Timespan.java
+++ pljava-api/src/main/java/org/postgresql/pljava/adt/Timespan.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Tada AB and other contributors, as listed below.
+ * Copyright (c) 2022-2025 Tada AB and other contributors, as listed below.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the The BSD 3-Clause License
@@ -12,6 +12,7 @@
 package org.postgresql.pljava.adt;
 
 import java.util.EnumSet;
+import static java.util.Collections.unmodifiableSet;
 import static java.util.EnumSet.of;
 import static java.util.EnumSet.noneOf;
 import static java.util.EnumSet.range;
@@ -19,6 +20,15 @@ import java.util.OptionalInt;
 import java.util.Set;
 
 import org.postgresql.pljava.Adapter.Contract;
+
+/*
+ * For the javadoc:
+ */
+import java.time.Duration;
+import java.time.Period;
+import java.time.chrono.ChronoPeriod;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
 
 /**
  * Container for abstract-type functional interfaces in PostgreSQL's
@@ -34,38 +44,63 @@ public interface Timespan
 	 * A type modifier can specify field-presence bits, and precision (number of
 	 * seconds digits to the right of the decimal point). An empty fields set
 	 * indicates that fields were not specified.
+	 *<h2>Infinitely negative or positive intervals</h2>
+	 *<p>
+	 * Starting with PostgreSQL 17, intervals whose three components are
+	 * {@code (Long.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE)} or
+	 * {@code (Long.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE)} have the
+	 * semantics of infinitely negative or positive intervals, respectively.
+	 * In PostgreSQL versions before 17, they are simply the most negative or
+	 * positive representable finite intervals.
 	 *<h2>Why no reference implementation?</h2>
 	 *<p>
 	 * The types in the {@link Datetime Datetime} interface come with reference
 	 * implementations returning Java's JSR310 {@code java.time} types.
 	 *<p>
-	 * For PostgreSQL {@code INTERVAL}, there are two candidate JSR310 types,
-	 * {@code Period} and {@code Duration}, each of which would be appropriate
-	 * for a different subset of PostgreSQL {@code INTERVAL} values.
+	 * For PostgreSQL {@code INTERVAL}, there are two candidate JSR310 concrete
+	 * types, {@link Period Period} and {@link Duration Duration}, each of which
+	 * would be appropriate for a different subset of PostgreSQL
+	 * {@code INTERVAL} values.
 	 *<p>
 	 * {@code Period} is appropriate for the months and days components.
 	 * A {@code Period} treats the length of a day as subject to daylight
 	 * adjustments following time zone rules, as does PostgreSQL.
 	 *<p>
 	 * {@code Duration} is suitable for the sub-day components. It also allows
-	 * access to a "day" field, but treats that as having invariant 24-hour
-	 * width.
+	 * access to a "day" field, but treats that field, unlike PostgreSQL, as
+	 * having invariant 24-hour width.
 	 *<p>
-	 * Both share the superinterface {@code TemporalAmount}. That interface
-	 * itself is described as "a framework-level interface that should not
-	 * be widely used in application code", recommending instead that new
+	 * Both share the superinterface {@link TemporalAmount TemporalAmount}. That
+	 * interface itself is described as "a framework-level interface that should
+	 * not be widely used in application code", recommending instead that new
 	 * concrete types can be created that implement it.
 	 *<p>
-	 * In the datatype library that comes with the PGJDBC-NG driver, there is
-	 * a class {@code com.impossibl.postgres.api.data.Interval} that takes that
-	 * approach exactly; it implements {@code TemporalAmount} and represents
-	 * all three components of the PostgreSQL interval with their PostgreSQL
-	 * semantics. An application with that library available could use an
-	 * implementation of this functional interface that would return instances
-	 * of that class.
+	 * PostgreSQL's {@code INTERVAL} could be represented by a concrete type
+	 * that implements {@code TemporalAmount} or, preferably (because its days
+	 * and months components are subject to rules of a chronology), its
+	 * subinterface {@link ChronoPeriod ChronoPeriod}. The most natural such
+	 * implementation would have {@link TemporalAmount#getUnits getUnits} return
+	 * {@link ChronoUnit#MONTHS MONTHS}, {@link ChronoUnit#DAYS DAYS}, and
+	 * {@link ChronoUnit#MICROS MICROS}, except for instances representing the
+	 * infinitely negative or positive intervals and using the unit
+	 * {@link ChronoUnit#FOREVER FOREVER} with a negative or positive value.
 	 *<p>
-	 * The PGJDBC driver includes a {@code org.postgresql.util.PGInterval} class
-	 * for the same purpose; that one does not derive from any JSR310 type.
+	 * In the datatype library that comes with the PGJDBC-NG driver, there is
+	 * a class {@code com.impossibl.postgres.api.data.Interval} that does
+	 * implement {@code TemporalAmount} (but not {@code ChronoPeriod}) and
+	 * internally segregates the PostgreSQL {@code INTERVAL} components into
+	 * a {@code Period} and a {@code Duration}. An application with that library
+	 * available could use an implementation of this functional interface that
+	 * would return instances of that class. As of PGJDBC-NG 0.8.9, the class
+	 * does not seem to have a representation for the PostgreSQL 17 infinite
+	 * intervals. Its {@code getUnits} method returns a longer list of units
+	 * than needed to naturally represent the PostgreSQL type.
+	 *<p>
+	 * The PGJDBC driver includes the {@code org.postgresql.util.PGInterval}
+	 * class for the same purpose; that one does not derive from any JSR310
+	 * type. As of PGJDBC 42.7.5, it does not explicitly represent infinite
+	 * intervals, and also has an internal state split into more units than the
+	 * natural representation would require.
 	 *<h3>Related notes from the ISO SQL/XML specification</h3>
 	 *<p>
 	 * SQL/XML specifies how to map SQL {@code INTERVAL} types and values to
@@ -73,8 +108,11 @@ public interface Timespan
 	 * {@code xs:dayTimeDuration}, which were added in XML Schema 1.1 as
 	 * distinct subtypes of the broader {@code xs:duration} type from XML Schema
 	 * 1.0. That Schema 1.0 supertype has a corresponding class in the standard
-	 * Java library, {@code javax.xml.datatype.Duration}, so an implementation
-	 * of this functional interface returning that type would also be easy.
+	 * Java library,
+	 * {@link javax.xml.datatype.Duration javax.xml.datatype.Duration}, so
+	 * an implementation of this functional interface to return that type would
+	 * also be easy. It would not, however, represent PostgreSQL 17 infinitely
+	 * negative or positive intervals.
 	 *<p>
 	 * These XML Schema types do not perfectly align with the PostgreSQL
 	 * {@code INTERVAL} type, because they group the day with the sub-day
@@ -122,24 +160,32 @@ public interface Timespan
 			YEAR, MONTH, DAY, HOUR, MINUTE, SECOND
 		}
 
-		EnumSet<Field>   YEAR = of(Field.YEAR);
-		EnumSet<Field>  MONTH = of(Field.MONTH);
-		EnumSet<Field>    DAY = of(Field.DAY);
-		EnumSet<Field>   HOUR = of(Field.HOUR);
-		EnumSet<Field> MINUTE = of(Field.MINUTE);
-		EnumSet<Field> SECOND = of(Field.SECOND);
+		Set<Field>   YEAR = unmodifiableSet(of(Field.YEAR));
+		Set<Field>  MONTH = unmodifiableSet(of(Field.MONTH));
+		Set<Field>    DAY = unmodifiableSet(of(Field.DAY));
+		Set<Field>   HOUR = unmodifiableSet(of(Field.HOUR));
+		Set<Field> MINUTE = unmodifiableSet(of(Field.MINUTE));
+		Set<Field> SECOND = unmodifiableSet(of(Field.SECOND));
 
-		EnumSet<Field>   YEAR_TO_MONTH  = range(Field.YEAR, Field.MONTH);
-		EnumSet<Field>    DAY_TO_HOUR   = range(Field.DAY,  Field.HOUR);
-		EnumSet<Field>    DAY_TO_MINUTE = range(Field.DAY,  Field.MINUTE);
-		EnumSet<Field>    DAY_TO_SECOND = range(Field.DAY,  Field.SECOND);
-		EnumSet<Field>   HOUR_TO_MINUTE = range(Field.HOUR, Field.MINUTE);
-		EnumSet<Field>   HOUR_TO_SECOND = range(Field.HOUR, Field.SECOND);
-		EnumSet<Field> MINUTE_TO_SECOND = range(Field.HOUR, Field.SECOND);
+		Set<Field>   YEAR_TO_MONTH  =
+							unmodifiableSet(range(Field.YEAR, Field.MONTH));
+		Set<Field>    DAY_TO_HOUR   =
+							unmodifiableSet(range(Field.DAY,  Field.HOUR));
+		Set<Field>    DAY_TO_MINUTE =
+							unmodifiableSet(range(Field.DAY,  Field.MINUTE));
+		Set<Field>    DAY_TO_SECOND =
+							unmodifiableSet(range(Field.DAY,  Field.SECOND));
+		Set<Field>   HOUR_TO_MINUTE =
+							unmodifiableSet(range(Field.HOUR, Field.MINUTE));
+		Set<Field>   HOUR_TO_SECOND =
+							unmodifiableSet(range(Field.HOUR, Field.SECOND));
+		Set<Field> MINUTE_TO_SECOND =
+							unmodifiableSet(range(Field.HOUR, Field.SECOND));
 
-		Set<EnumSet<Field>> ALLOWED_FIELDS =
+		Set<Set<Field>> ALLOWED_FIELDS =
 			Set.of(
-				noneOf(Field.class), YEAR, MONTH, DAY, HOUR, MINUTE, SECOND,
+				unmodifiableSet(noneOf(Field.class)),
+				YEAR, MONTH, DAY, HOUR, MINUTE, SECOND,
 				YEAR_TO_MONTH, DAY_TO_HOUR, DAY_TO_MINUTE, DAY_TO_SECOND,
 				HOUR_TO_MINUTE, HOUR_TO_SECOND, MINUTE_TO_SECOND);
 
@@ -213,7 +259,7 @@ public interface Timespan
 			 * a valid type modifier, when this is specified, <var>fields</var>
 			 * must either include {@code SECONDS}, or be unspecified.
 			 */
-			Interval<T> modify(EnumSet<Field> fields, OptionalInt precision);
+			Interval<T> modify(Set<Field> fields, OptionalInt precision);
 		}
 	}
 }
