87bd72c97b67bac7dd66c6df77b441d05e8e8388
Chapman Flack <chap@anastigmatix.net>
2025-03-27 11:52:42 -0400

    Refresh the 1.7 javadoc preview

---
+++ preview1.7/pljava-examples/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.postgresql.pljava.example"},{"l":"org.postgresql.pljava.example.annotation"},{"l":"org.postgresql.pljava.example.polyglot"},{"l":"org.postgresql.pljava.example.saxon"}];updateSearchResults();
