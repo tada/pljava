cb1933316176c3cb45188035bb2cf0f37f882088
Chapman Flack <chap@anastigmatix.net>
2025-04-05 18:43:23 -0400

    API for PLJavaBasedLanguage supporting triggers

--- pljava-api/src/main/java/org/postgresql/pljava/PLJavaBasedLanguage.java
+++ pljava-api/src/main/java/org/postgresql/pljava/PLJavaBasedLanguage.java
@@ -12,20 +12,30 @@
 package org.postgresql.pljava;
 
 import java.sql.SQLException;
+import java.sql.SQLSyntaxErrorException;
 
 import org.postgresql.pljava.model.ProceduralLanguage; // javadoc
 import org.postgresql.pljava.model.ProceduralLanguage.PLJavaBased;
 import org.postgresql.pljava.model.RegProcedure;
 import org.postgresql.pljava.model.RegProcedure.Call;
+import org.postgresql.pljava.model.RegProcedure.Call.Context.TriggerData; // jvd
 import org.postgresql.pljava.model.RegProcedure.Lookup;
+import org.postgresql.pljava.model.RegType;
+import org.postgresql.pljava.model.Trigger;
 import org.postgresql.pljava.model.TupleTableSlot; // javadoc
+
+import org.postgresql.pljava.annotation.Trigger.Called; // javadoc
+import org.postgresql.pljava.annotation.Trigger.Event; // javadoc
+import org.postgresql.pljava.annotation.Trigger.Scope; // javadoc
 
 /**
  * Interface for a procedural language on PL/Java infrastructure.
  *<p>
  * An implementing class does not implement this interface directly, but rather
  * implements one or both of the subinterfaces {@link InlineBlocks InlineBlocks}
- * and {@link Routines Routines}. It must have a public constructor with a
+ * and {@link Routines Routines}. A language that implements {@code Routines}
+ * may also implement {@link Triggers Triggers}. The implementing class must
+ * have a public constructor with a
  * {@link ProceduralLanguage ProceduralLanguage} parameter, which it may ignore,
  * or use to determine the name, oid, accessibility, or other details of the
  * declared PostgreSQL language the handler class has been instantiated for.
@@ -83,7 +93,10 @@ public interface PLJavaBasedLanguage
 		 * <var>checkBody</var> in that case).
 		 *<p>
 		 * This method should throw an informative exception for any check that
-		 * fails, otherwise returning normally.
+		 * fails, otherwise returning normally. Unless there is a more-specific
+		 * choice, {@link SQLSyntaxErrorException} with {@code SQLState}
+		 * {@code 42P13} corresponds to PostgreSQL's
+		 * {@code invalid_function_definition}.
 		 *<p>
 		 * Checks that are helpful at routine-creation time, but not essential
 		 * to correctness of {@code prepare}, can be made in
@@ -123,7 +136,10 @@ public interface PLJavaBasedLanguage
 		 * {@link #essentialChecks essentialChecks} has completed normally.
 		 *<p>
 		 * This method should throw an informative exception for any check that
-		 * fails, otherwise returning normally.
+		 * fails, otherwise returning normally. Unless there is a more-specific
+		 * choice, {@link SQLSyntaxErrorException} with {@code SQLState}
+		 * {@code 42P13} corresponds to PostgreSQL's
+		 * {@code invalid_function_definition}.
 		 *<p>
 		 * Checks of conditions essential to correctness of {@code prepare}
 		 * must be made in {@code essentialChecks}.
@@ -183,6 +199,86 @@ public interface PLJavaBasedLanguage
 	}
 
 	/**
+	 * To be implemented by a language that supports triggers.
+	 *<p>
+	 * The methods of this interface will be called, instead of those declared
+	 * in {@link Routines Routines}, for any function declared with return type
+	 * {@link RegType#TRIGGER TRIGGER}. If a language does not implement
+	 * this interface, any attempt to validate or use such a function will incur
+	 * an exception.
+	 */
+	public interface Triggers extends PLJavaBasedLanguage
+	{
+		/**
+		 * Performs the essential validation checks on a proposed
+		 * trigger function.
+		 *<p>
+		 * See {@link Routines#essentialChecks Routines.essentialChecks} for
+		 * details on what to check here.
+		 *<p>
+		 * Any <var>subject</var> passed to this method is already known to be
+		 * a function with no declared parameters and a non-set return type of
+		 * {@link RegType#TRIGGER TRIGGER}.
+		 *<p>
+		 * This default implementation checks nothing further.
+		 */
+		default void essentialTriggerChecks(
+			RegProcedure<PLJavaBased> subject, boolean checkBody)
+		throws SQLException
+		{
+		}
+
+		/**
+		 * Performs additional validation checks on a proposed trigger function.
+		 *<p>
+		 * See {@link Routines#additionalChecks Routines.additionalChecks} for
+		 * details on what to check here.
+		 *<p>
+		 * Any <var>subject</var> passed to this method is already known to be
+		 * a function with no declared parameters and a non-set return type of
+		 * {@link RegType#TRIGGER TRIGGER}, and to have passed the
+		 * {@link #essentialTriggerChecks essentialTriggerChecks}.
+		 *<p>
+		 * This default implementation checks nothing further.
+		 */
+		default void additionalTriggerChecks(
+			RegProcedure<PLJavaBased> subject, boolean checkBody)
+		throws SQLException
+		{
+		}
+
+		/**
+		 * Prepares a template for a call of the trigger function
+		 * <var>target</var>.
+		 *<p>
+		 * See {@link Routines#prepare Routines.prepare} for background on
+		 * what to do here.
+		 *<p>
+		 * This method should return a {@link TriggerTemplate TriggerTemplate},
+		 * which may encapsulate any useful precomputed values based on
+		 * the catalog information this method consulted.
+		 *<p>
+		 * Any <var>target</var> passed to this method is already known to be
+		 * a function with no declared parameters and a non-set return type of
+		 * {@link RegType#TRIGGER TRIGGER}, and to have passed the
+		 * {@link #essentialTriggerChecks essentialTriggerChecks}.
+		 *<p>
+		 * The template, when its {@link TriggerTemplate#specialize specialize}
+		 * method is invoked on a {@link Trigger Trigger} instance, should
+		 * return a {@link TriggerFunction TriggerFunction} that can be invoked
+		 * on a {@link TriggerData TriggerData} instance.
+		 *<p>
+		 * The template may generate a {@code TriggerFunction} that encapsulates
+		 * specifics of the {@code Trigger} such as its target table, name,
+		 * arguments, enabled events, scope, and columns of interest.
+		 * A {@code TriggerFunction} will not be invoked for any trigger except
+		 * the one passed to the {@code specialize} call that returned it.
+		 */
+		TriggerTemplate prepareTrigger(RegProcedure<PLJavaBased> target)
+		throws SQLException;
+	}
+
+	/**
 	 * To be implemented by a language that supports inline code blocks.
 	 *<h2>Transaction control</h2>
 	 *<p>
@@ -236,6 +332,40 @@ public interface PLJavaBasedLanguage
 	}
 
 	/**
+	 * The result of a {@link TriggerTemplate#specialize specialize} call on
+	 * a {@link TriggerTemplate TriggerTemplate}.
+	 *<p>
+	 * An instance can incorporate whatever can be precomputed based on the
+	 * specific {@link Trigger Trigger} that was passed to {@code specialize}.
+	 * Its {@link #apply apply} method will then be invoked to act on
+	 * the {@link TriggerData TriggerData} and produce the results each time
+	 * that trigger fires.
+	 */
+	@FunctionalInterface
+	public interface TriggerFunction
+	{
+		/**
+		 * Actually executes the prepared and specialized
+		 * {@code TriggerFunction}, with the triggering data available in
+		 * <var>triggerData</var>.
+		 *<p>
+		 * The return value, ignored for an {@link Called#AFTER AFTER} trigger,
+		 * and restricted to null for any
+		 * {@link Called#BEFORE BEFORE} {@link Scope#STATEMENT STATEMENT}
+		 * trigger, can influence the triggering operation for other types
+		 * of triggers. To permit the operation with no changes by the trigger,
+		 * return exactly {@link TriggerData#triggerTuple triggerTuple} (for
+		 * a trigger on {@link Event#INSERT INSERT} or
+		 * {@link Event#DELETE DELETE}), or exactly
+		 * {@link TriggerData#newTuple newTuple} (for a trigger on
+		 * {@link Event#UPDATE UPDATE}). To suppress the triggering operation,
+		 * return null.
+		 * @return a TupleTableSlot, or null
+		 */
+		TupleTableSlot apply(TriggerData triggerData) throws SQLException;
+	}
+
+	/**
 	 * The result of a {@link Routines#prepare prepare} call on a PL/Java-based
 	 * routine.
 	 *<p>
@@ -243,7 +373,7 @@ public interface PLJavaBasedLanguage
 	 * routine as passed to {@code prepare}, and may encapsulate any values that
 	 * can be precomputed from that information alone. Its
 	 * {@link #specialize specialize} method will be called, passing information
-	 * specific to a call site, to obtain a {@link Routine}.
+	 * specific to a call site, to obtain a {@link Routine Routine}.
 	 */
 	@FunctionalInterface
 	public interface Template
@@ -264,5 +394,40 @@ public interface PLJavaBasedLanguage
 		 * {@code "any"} type.
 		 */
 		Routine specialize(Lookup flinfo) throws SQLException;
+	}
+
+	/**
+	 * The result of a {@link Triggers#prepareTrigger prepareTrigger} call on
+	 * a PL/Java-based trigger function.
+	 *<p>
+	 * An instance should depend only on the static catalog information for the
+	 * function as passed to {@code prepareTrigger}, and may encapsulate any
+	 * values that can be precomputed from that information alone. Its
+	 * {@link #specialize specialize} method will be called, passing information
+	 * specific to one trigger, to obtain a
+	 * {@link TriggerFunction TriggerFunction}.
+	 */
+	@FunctionalInterface
+	public interface TriggerTemplate
+	{
+		/**
+		 * Given the specifics of one {@link Trigger Trigger}, specialize
+		 * this template into a {@link TriggerFunction TriggerFunction} that
+		 * will handle calls through this trigger.
+		 *<p>
+		 * Typical activities for {@code specialize} would be to consult
+		 * <var>trigger</var>'s {@link Trigger#name name},
+		 * {@link Trigger#relation relation}, {@link Trigger#called called},
+		 * {@link Trigger#events events}, {@link Trigger#scope scope},
+		 * {@link Trigger#arguments arguments}, and
+		 * {@link Trigger#columns columns} to
+		 * determine the kind of trigger it is, and fold those values into
+		 * the returned {@code TriggerFunction}.
+		 *<p>
+		 * This stage is well suited for checking that the characteristics of
+		 * the trigger (events, scope, when called, arguments, column types of
+		 * the target table) conform to what the trigger function can handle.
+		 */
+		TriggerFunction specialize(Trigger trigger) throws SQLException;
 	}
 }
